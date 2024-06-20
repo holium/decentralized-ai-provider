152979b6c801aa332530dcb074fdf954572e672e
Tenari <djz24@case.edu>
2024-06-20 15:00:34 -0500

    deduplicate contract address

--- modules/ai_provider/worker/src/chain.rs
+++ modules/ai_provider/worker/src/chain.rs
@@ -1,16 +1,11 @@
 /// This file contains the logic for interacting with the AppRegistry contract.
 use alloy_sol_types::{sol, SolCall, SolValue};
 use kinode_process_lib::{
-    eth::{Address as EthAddress, Provider, TransactionInput, TransactionRequest, U64},
+    eth::{Provider, TransactionInput, TransactionRequest, U64},
     println,
 };
 use serde::{Deserialize, Serialize};
-
-// 0xa51c1fc2f0d1a1b8494ed1fe312d7c3a78ed91c0
-static CONTRACT_ADDRESS: EthAddress = EthAddress::new([
-    0xa5,0x1c, 0x1f, 0xc2, 0xf0, 0xd1, 0xa1, 0xb8, 0x49, 0x4e, 0xd1, 0xfe, 0x31, 0x2d, 0x7c, 0x3a, 0x78, 0xed, 0x91, 0xc0, 
-//    0x22, 0x79, 0xb7, 0xa0, 0xa6, 0x7d, 0xb3, 0x72, 0x99, 0x6a, 0x5f, 0xab, 0x50, 0xd9, 0x1e, 0xaa, 0x73, 0xd2, 0xeb, 0xe6,
-]);
+use shared::CONTRACT_ADDRESS;
 
 sol! {
     #[derive(Debug, Serialize, Deserialize)]
